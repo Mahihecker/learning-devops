828d72273d777e1f5c03b757bd3504aa3477b989
Ubuntu <mahreen@Mahreen.thleoodkfk1ebavv1vutii4tjg.bx.internal.cloudapp.net>
2024-08-07 10:01:48 +0000

    shell-scripting

---
+++ node-health.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+##########################################################
+#Author:Mahreen Shakeel
+#Date: 8/7/2024
+#Discription: thi script it for the output of nodeheatlh
+#Version:v1
+##########################################################
+
+
+set -exo pipefail
+
+df -h
+free -g
+nproc
+top
+ps
+
